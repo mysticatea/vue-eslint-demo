c311825e0549d2a044e024a4ecbe5e1b1c9cf25f
Toru Nagashima <star.ctor@gmail.com>
2017-11-30 14:49:30 +0900

    Chore: add deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,58 @@
+/*eslint-env node */
+
+const sh = require("shelljs")
+
+main()
+
+/**
+ * Main.
+ * @returns {void}
+ */
+function main() {
+    exec("npm run build")
+    exec("git checkout gh-pages")
+
+    if (String(sh.cat("dist/versions.json")) === String(sh.cat("versions.json"))) {
+        sh.echo("No update.")
+        exec("git checkout -")
+        return
+    }
+
+    rm("-rf", "vs", "index.*")
+    cp("dist/*", ".")
+    exec("git add -A")
+    exec("git commit -m \"Update: website\"")
+    exec("git push")
+    exec("git checkout -")
+}
+
+/**
+ * Execute `cp` command.
+ * @returns {void}
+ */
+function cp(...args) {
+    sh.echo(`> cp ${args.join(" ")}`)
+    sh.cp(...args)
+}
+
+/**
+ * Execute a command.
+ * @param {string} command The command to execute.
+ * @returns {void}
+ */
+function exec(command) {
+    sh.echo(`> ${command}`)
+    const code = sh.exec(command, { stdio: "inherit" }).code
+    if (code) {
+        throw new Error(`Exited with ${code}.`)
+    }
+}
+
+/**
+ * Execute `rm` command.
+ * @returns {void}
+ */
+function rm(...args) {
+    sh.echo(`> rm ${args.join(" ")}`)
+    sh.rm(...args)
+}
---
+++ scripts/make-versions.js
@@ -0,0 +1,13 @@
+/*eslint-env node */
+
+const fs = require("fs")
+
+fs.writeFileSync(
+    "dist/versions.json",
+    JSON.stringify({
+        "eslint": require("eslint/package.json").version,
+        "eslint-plugin-vue": require("eslint-plugin-vue/package.json").version,
+        "vue-eslint-demo": require("../package.json").version,
+        "vue-eslint-parser": require("vue-eslint-parser/package.json").version,
+    })
+)
