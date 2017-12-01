dabda8f5ed52b59b62e6fad0039f3b9ce56834a5
Toru Nagashima <star.ctor@gmail.com>
2017-12-01 14:22:15 +0900

    Chore: remove unused files from dist

---
+++ scripts/copy-monaco.js
@@ -0,0 +1,36 @@
+"use strict"
+
+const childProcess = require("child_process")
+const args = process.argv.slice(2)
+const root = "node_modules/monaco-editor/min/vs/"
+const files = [
+    "base/worker/workerMain.js",
+    "basic-languages/src/html.js",
+    "editor/editor.main.css",
+    "editor/editor.main.js",
+    "editor/editor.main.nls.js",
+    "editor/contrib/suggest/browser/media/String_16x.svg",
+    "editor/contrib/suggest/browser/media/String_inverse_16x.svg",
+    "editor/standalone/browser/quickOpen/symbol-sprite.svg",
+    "language/css/cssMode.js",
+    "language/css/cssWorker.js",
+    "language/html/htmlMode.js",
+    "language/html/htmlWorker.js",
+    "language/typescript/lib/typescriptServices.js",
+    "language/typescript/src/mode.js",
+    "language/typescript/src/worker.js",
+    "loader.js",
+]
+const binFile = "node_modules/cpx/bin/index.js"
+const glob = `${root}{${files.join(",")}}`
+const dist = "dist/vs"
+
+console.log("> cpx", glob, dist, ...args)
+const cp = childProcess.spawn(
+    process.execPath,
+    [binFile, glob, dist, ...args],
+    { stdio: "inherit" }
+)
+cp.on("exit", (exitCode) => {
+    process.exitCode = exitCode
+})
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-/*eslint-env node */
+"use strict"
 
 const sh = require("shelljs")
 
