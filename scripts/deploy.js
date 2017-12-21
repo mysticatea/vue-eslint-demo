e51331c2d13a1e56256eb994a375fce7504df511
Toru Nagashima <star.ctor@gmail.com>
2017-12-21 09:39:50 +0900

    Chore: improve deploy script
    - needs rebuild after version bump because the preversion script is in the previous version.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,15 @@
 "use strict"
 
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
 const sh = require("shelljs")
 const version = require("../package.json").version
-const skipBuild = (process.argv.indexOf("--skip-build") >= 0)
 
-main()
-
-/**
- * Main.
- * @returns {void}
- */
-function main() {
-    if (!skipBuild) {
-        exec(`git checkout v${version}`)
-        exec("npm run build")
-    }
-    exec("git checkout gh-pages")
-
-    if (String(sh.cat("dist/versions.json")) !== String(sh.cat("versions.json"))) {
-        rm("-rf", "vs", "index.*")
-        cp("-r", "dist/*", ".")
-        exec("git add -A")
-        exec("git commit -m \"Update: website\"")
-        exec("git push")
-    }
-
-    exec("git checkout master")
-}
+//------------------------------------------------------------------------------
+// Helpers
+//------------------------------------------------------------------------------
 
 /**
  * Execute `cp` command.
@@ -35,6 +18,15 @@ function main() {
 function cp(...args) {
     sh.echo(`> cp ${args.join(" ")}`)
     sh.cp(...args)
+}
+
+/**
+ * Execute `rm` command.
+ * @returns {void}
+ */
+function rm(...args) {
+    sh.echo(`> rm ${args.join(" ")}`)
+    sh.rm(...args)
 }
 
 /**
@@ -50,11 +42,20 @@ function exec(command) {
     }
 }
 
-/**
- * Execute `rm` command.
- * @returns {void}
- */
-function rm(...args) {
-    sh.echo(`> rm ${args.join(" ")}`)
-    sh.rm(...args)
+//------------------------------------------------------------------------------
+// Main
+//------------------------------------------------------------------------------
+
+exec(`git checkout v${version}`)
+exec("npm run build")
+exec("git checkout gh-pages")
+
+if (String(sh.cat("dist/versions.json")) !== String(sh.cat("versions.json"))) {
+    rm("-rf", "vs", "index.*")
+    cp("-r", "dist/*", ".")
+    exec("git add -A")
+    exec("git commit -m \"Update: website\"")
+    exec("git push")
 }
+
+exec("git checkout master")
