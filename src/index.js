d2630a4005ff4798caa38cdaf79489c1eadd614b
Toru Nagashima <star.ctor@gmail.com>
2017-12-26 21:23:34 +0900

    Chore: simplify

--- src/index.js
+++ src/index.js
@@ -2,25 +2,16 @@ import Vue from "vue"
 import App from "./app.vue"
 
 const app = new Vue({
-    computed: {
-        updateReady: {
-            get() {
-                return this.$refs.app.showUpdateReadyToast
-            },
-            set(value) {
-                this.$refs.app.showUpdateReadyToast = value
-            },
+    el: "#main",
+    methods: {
+        showUpdateReadyToast() {
+            this.$refs.app.showUpdateReadyToast = true
         },
     },
     render: (h) => h(App, { ref: "app" }),
 })
-app.$mount("#main")
 
 // Check update.
-window.addEventListener("load", () => {
-    applicationCache.addEventListener("updateready", () => {
-        if (applicationCache.status === applicationCache.UPDATEREADY) {
-            app.updateReady = true
-        }
-    })
+applicationCache.addEventListener("updateready", () => {
+    app.showUpdateReadyToast()
 })
