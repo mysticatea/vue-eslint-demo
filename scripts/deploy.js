79266b5e9436ac0c4a4b70ad29e224374dcddb39
Toru Nagashima <star.ctor@gmail.com>
2017-12-26 20:21:21 +0900

    Chore: add .travis.yml to auto-deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,6 +6,7 @@
 
 const sh = require("shelljs")
 const version = require("../package.json").version
+const ATOKEN = process.env.ATOKEN //eslint-disable-line no-process-env
 
 //------------------------------------------------------------------------------
 // Helpers
@@ -46,16 +47,30 @@ function exec(command) {
 // Main
 //------------------------------------------------------------------------------
 
+// Build.
 exec(`git checkout v${version}`)
 exec("npm run build")
+
+// Load gh-pages.
+if (ATOKEN) {
+    exec("git fetch --depth=1 https://github.com/mysticatea/vue-eslint-demo.git gh-pages:gh-pages")
+}
 exec("git checkout gh-pages")
 
-if (String(sh.cat("dist/versions.json")) !== String(sh.cat("versions.json"))) {
+// Check versions.
+const oldVersions = String(sh.cat("versions.json"))
+const newVersions = String(sh.cat("dist/versions.json"))
+sh.echo(`OLD: ${oldVersions}`)
+sh.echo(`NEW: ${newVersions}`)
+
+// Deploy.
+if (newVersions !== oldVersions) {
     rm("-rf", "vs", "index.*")
     cp("-r", "dist/*", ".")
     exec("git add -A")
     exec("git commit -m \"Update: website\"")
-    exec("git push")
+    exec(`git push${ATOKEN ? ` https://mysticatea:${ATOKEN}@github.com/mysticatea/vue-eslint-demo.git gh-pages:gh-pages` : ""}`)
 }
 
+// Back to master.
 exec("git checkout master")
