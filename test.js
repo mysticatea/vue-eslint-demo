94a311796e0e2c6e4139ae77056355697829646b
Toru Nagashima <star.ctor@gmail.com>
2018-10-23 17:10:16 +0900

    Update website

--- test.js
+++
@@ -1,9 +0,0 @@
-const eslint = require("eslint")
-const linter = new eslint.Linter()
-const rules = linter.getRules()
-
-for (const [id, rule] of rules) {
-    if (rule.meta.docs.recommended) {
-        console.log(id)
-    }
-}
