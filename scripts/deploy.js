e15b2037df7eb092e488164ade9798dbfebaa9e1
Toru Nagashima <star.ctor@gmail.com>
2018-10-23 17:06:16 +0900

    Chore: update build script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,34 +1,12 @@
 "use strict"
 
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
-
-const sh = require("shelljs")
+const { spawn } = require("child_process")
+const path = require("path")
+const fs = require("fs-extra")
 const version = require("../package.json").version
 const ATOKEN = process.env.ATOKEN
-
-//------------------------------------------------------------------------------
-// Helpers
-//------------------------------------------------------------------------------
-
-/**
- * Execute `cp` command.
- * @returns {void}
- */
-function cp(...args) {
-    sh.echo(`> cp ${args.join(" ")}`)
-    sh.cp(...args)
-}
-
-/**
- * Execute `rm` command.
- * @returns {void}
- */
-function rm(...args) {
-    sh.echo(`> rm ${args.join(" ")}`)
-    sh.rm(...args)
-}
+const BUILD_ROOT = path.resolve(__dirname, "../dist")
+const DEPLOY_ROOT = path.resolve(__dirname, "..")
 
 /**
  * Execute a command.
@@ -36,52 +14,90 @@ function rm(...args) {
  * @returns {void}
  */
 function exec(command) {
-    sh.echo(`> ${command}`)
-    const code = sh.exec(command, { stdio: "inherit" }).code
-    if (code) {
-        throw new Error(`Exited with ${code}.`)
-    }
+    console.log(`> ${command}`)
+    return new Promise((resolve, reject) => {
+        const cp = spawn(command, [], { shell: true, stdio: "inherit" })
+
+        cp.on("close", code => {
+            if (code) {
+                reject(new Error(`Exited with ${code}.`))
+            } else {
+                resolve()
+            }
+        })
+    })
 }
 
-//------------------------------------------------------------------------------
 // Main
-//------------------------------------------------------------------------------
+;(async () => {
+    console.log("BUILD")
+    await exec(`git checkout v${version}`)
+    await exec("npm run -s build")
 
-// Build.
-exec(`git checkout v${version}`)
-exec("npm run build")
+    console.log("LOAD GH-PAGES")
+    if (ATOKEN) {
+        await exec(
+            "git fetch --depth=1 https://github.com/mysticatea/vue-eslint-demo.git gh-pages:gh-pages",
+        )
+        await exec("git checkout gh-pages")
+    } else {
+        await exec("git checkout gh-pages")
+        await exec("git pull")
+    }
 
-// Load gh-pages.
-if (ATOKEN) {
-    exec(
-        "git fetch --depth=1 https://github.com/mysticatea/vue-eslint-demo.git gh-pages:gh-pages",
-    )
-    exec("git checkout gh-pages")
-} else {
-    exec("git checkout gh-pages")
-    exec("git pull")
-}
+    console.log("CHECK VERSIONS")
+    const oldVersions = await fs.readFile("versions.json", "utf8")
+    const newVersions = await fs.readFile("dist/versions.json", "utf8")
+    console.log(`OLD: ${oldVersions}`)
+    console.log(`NEW: ${newVersions}`)
 
-// Check versions.
-const oldVersions = String(sh.cat("versions.json"))
-const newVersions = String(sh.cat("dist/versions.json"))
-sh.echo(`OLD: ${oldVersions}`)
-sh.echo(`NEW: ${newVersions}`)
+    // Deploy.
+    if (newVersions !== oldVersions) {
+        console.log("CLEAN")
+        for (const filename of await fs.readdir(DEPLOY_ROOT)) {
+            const stat = await fs.stat(filename)
+            if (!stat.isFile() || filename.startsWith(".")) {
+                continue
+            }
 
-// Deploy.
-if (newVersions !== oldVersions) {
-    rm("-rf", "vs", "index.*")
-    cp("-r", "dist/*", ".")
-    exec("git add -A")
-    exec('git commit -m "Update: website"')
-    exec(
-        `git push${
-            ATOKEN
-                ? ` https://mysticatea:${ATOKEN}@github.com/mysticatea/vue-eslint-demo.git gh-pages:gh-pages`
-                : ""
-        }`,
-    )
-}
+            console.log(`> rm ${filename}`)
+            await fs.unlink(filename)
+        }
 
-// Back to master.
-exec("git checkout master")
+        console.log("DEPLOY")
+        for (const filename of await fs.readdir(BUILD_ROOT)) {
+            console.log(`> mv dist/${filename} ${filename}`)
+            await fs.rename(
+                path.join(BUILD_ROOT, filename),
+                path.join(DEPLOY_ROOT, filename),
+            )
+        }
+
+        await exec("git add -A")
+        let updated = false
+        try {
+            await exec('git commit -m "Update: website"')
+            updated = true
+        } catch (_error) {
+            console.log("NO UPDATE")
+        }
+        if (updated) {
+            await exec(
+                `git push${
+                    ATOKEN
+                        ? ` https://mysticatea:${ATOKEN}@github.com/mysticatea/vue-eslint-demo.git gh-pages:gh-pages`
+                        : ""
+                }`,
+            )
+        }
+    } else {
+        console.log("NO UPDATE")
+    }
+
+    // Back to master.
+    await exec("git checkout master")
+    console.log("COMPLETE")
+})().catch(error => {
+    console.error(error.stack)
+    process.exitCode = 1
+})
